a434bdc68e5eb93d095b581ae2bdf998eec4ac68
해석 조 <haishuo@naver.com>
2022-04-13 09:16:42 +0900

    포인터연습

--- Cstudy2022/포인터연습3.c
+++ Cstudy2022/포인터연습3.c
@@ -2,13 +2,31 @@
 #include <stdio.h>
 
 int main() {
-	int* p;
-	int c[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	p = &c;
+	//int a = 10, b = 20, temp;
+	//temp = a;
+	//a = b;
+	//b = temp;
+	//printf("%d %d", a, b);
 
-	*(p + 4) = 10;
+	int* p; 
 
-	printf("%d", c[4]);
+	int c[5] = { 1,2,3,4,5 }; 
+	int i, temp = 0;
+    p = c; // 1000
+
+
+	printf("%x %x %x %x \n", p, &c, p+1, (&c)+1); // 1000 1000 1004 1020
+	
+
+	printf("%d %d %d\n", c[0], c[1], c[2]); // 1 2 3
+
+
+	printf("%p %p %p\n", c, c+1, c+2); // 1000 1004 1008
+	printf("%x %x %x\n", p, p + 1, p + 2); // 1000 1004 1008
+	printf("%d %d %d\n", *c, *(c+1), *(c+2)); // 1 2 3
+	printf("%p %p %p %p\n", c, &c, c+1, (&c)+1); // 1000 1000 1004 1020 
+	printf("%x %x %x\n", *p, *(p + 1), *(p + 2)); // 1 2 3
+	
 
 	return 0;
 }
