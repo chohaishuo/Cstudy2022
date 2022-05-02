928b87ea53bfac35f4cc2c9ae9e79adfe5df313e
해석 조 <haishuo@naver.com>
2022-05-02 11:43:37 +0900

    c문제3

---
+++ Cstudy2022/c시험3.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+
+	char phone[15];
+	scanf("%s", phone);
+
+
+	// ================================================
+	// 이 부분에 코드를 작성하세요.
+	// 포인터를 사용해 '-'를 공백으로 바꾸기
+	char* pp;
+	pp = phone;
+	int i;
+
+	for ( i = 0; i < 15; i++)
+	{
+		*pp++;
+		if (*pp == "-") {
+			*pp == " ";
+		}
+	}
+
+
+
+
+	//=================================================
+
+	printf("phone: %s", phone);
+}
