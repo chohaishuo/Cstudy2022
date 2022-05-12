b7c8cfe40bcc91800da4b93384828972e51629f9
해석 조 <haishuo@naver.com>
2022-05-12 14:31:59 +0900

    2차원배열

---
+++ Cstudy2022/2차원배열.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+	int a[2][3] = { {1,2,3}, {4,5,6} };
+
+
+
+	printf("%d %d \n", a, a+1); // 1000 1012
+	printf("%d %d \n", a[0], a[0]+1); // 1000 1004
+	printf("%d %d \n", *a[0],*(a[0] + 1)); // 1 2
+	printf("%d %d \n", a+1, *(a+1)); // 1012 1012
+
+
+
+
+
+	return 0;
+}
